bd0de2acf6eea0438970f84bf7e89e2eb5df1150
Ilya <126606689+MrGoroshic@users.noreply.github.com>
2024-10-17 18:18:33 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,6 @@
+count_of_completed_home_work = 12
+count_of_spent_hour = 1.5
+course_name = 'Pyton'
+time_for_complete_one_task = count_of_spent_hour / count_of_completed_home_work
+print("Курс:", course_name, ", всего задач:", count_of_completed_home_work,
+      ", затрачено часов:", count_of_spent_hour, ", среднее время выполнения:", time_for_complete_one_task)
